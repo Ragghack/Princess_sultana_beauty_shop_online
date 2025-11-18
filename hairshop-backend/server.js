dbbd94a648743d3328c880cd5d92035c9e2d56cd
Ragghack <draftragg@gmail.com>
2025-11-19 00:09:05 +0100

    new sever

--- hairshop-backend/server.js
+++ hairshop-backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors({
     origin: [
       'https://princess-sultana-beauty-shop-online.vercel.app',
+      'https://princess-sultana-beauty-shop-online.onrender.com',
         'http://127.0.0.1:5501', 
         'http://localhost:3000', 
         'http://localhost:5501',
