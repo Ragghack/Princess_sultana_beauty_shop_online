b95f28b396f51ef608ff552e76028a25967d197d
Ragghack <draftragg@gmail.com>
2026-08-12 23:04:35 +0100

    the app.js touch

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ const BACKEND_URL =
 
 const FRONTEND_URL =
   process.env.FRONTEND_URL ||
-  "https://princess-sultana-beauty-shop-online-g682p8r6d.vercel.app";
+  "https://princess-sultana-beauty-shop-online-sigma.vercel.app";
 
 /*
 |--------------------------------------------------------------------------
